33bb3b2d9a780306aaf95c39e893653a02c52dcb
Juanfrancoledesma <juanfledesma18@gmail.com>
2023-01-22 11:00:06 -0300

    deploy config

--- api/models/index.ts
+++ api/models/index.ts
@@ -11,7 +11,7 @@ const db:any = {};
 
 let sequelize:any;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env.DB_DEPLOY, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
