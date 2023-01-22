2dacad927fd49640923e73c58023ef5cbdab3d8f
Juanfrancoledesma <juanfledesma18@gmail.com>
2023-01-22 11:28:58 -0300

    deploy config 4

--- api/models/index.ts
+++ api/models/index.ts
@@ -5,17 +5,20 @@ const path = require('path');
 const Sequelize = require('sequelize');
 // const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.js')[env];
 const db:any = {};
 
 let sequelize:any;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env.DB_DEPLOY, config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {ssl: { rejectUnauthorized: false }});
-}
-
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env.DB_DEPLOY, config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+sequelize = new Sequelize({
+  connectionString: process.env.DB_DEPLOY,
+  ssl: { rejectUnauthorized: false }
+});
 fs
   .readdirSync(__dirname)
   .filter((file: string) => {
