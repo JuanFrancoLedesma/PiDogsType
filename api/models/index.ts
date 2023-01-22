0c3eb63d6bc6988a65a053bd31f137bc6fb1f3c0
Juanfrancoledesma <juanfledesma18@gmail.com>
2023-01-22 11:36:59 -0300

    deploy config 6

--- api/models/index.ts
+++ api/models/index.ts
@@ -9,6 +9,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db:any = {};
 
+console.log(config);
+
+
 let sequelize:any;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
